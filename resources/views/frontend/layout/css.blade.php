326bda7993eae80d44a371c382bbb57b23a8e86c
yuzgec <olcayy@gmail.com>
2025-02-20 15:13:55 +0300

    Fix

--- resources/views/frontend/layout/css.blade.php
+++ resources/views/frontend/layout/css.blade.php
@@ -1,4 +1,4 @@
 <link rel="shortcut icon" href="/{{ config('settings.siteFavIcon')}}" type="image/x-icon" />
 <link rel="apple-touch-icon" href="/{{ config('settings.siteFavIcon')}}">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
-<link rel="stylesheet" href="/frontend/css/style.css?ver=5">
+<link rel="stylesheet" href="/frontend/css/style.css?ver=6">
