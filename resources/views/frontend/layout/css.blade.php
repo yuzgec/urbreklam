a10c6326d78d15ae8d055f40d25781f5c09605e6
yuzgec <olcayy@gmail.com>
2025-02-21 18:04:24 +0300

    Fix

--- resources/views/frontend/layout/css.blade.php
+++ resources/views/frontend/layout/css.blade.php
@@ -2,3 +2,8 @@
 <link rel="apple-touch-icon" href="/{{ config('settings.siteFavIcon')}}">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
 <link rel="stylesheet" href="/frontend/css/style.css?ver=7">
+
+<link rel="alternate" hreflang="tr" href="https://urbanreklam.com/" />
+<link rel="alternate" hreflang="en" href="https://urbanreklam.com/en/" />
+<link rel="alternate" hreflang="ar" href="https://urbanreklam.com/sa/" />
+<link rel="alternate" hreflang="x-default" href="https://urbanreklam.com/" />
