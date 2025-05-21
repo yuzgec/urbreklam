9f0afd117fd5fcc6a2523b1b0d809379727f5b5f
yuzgec <olcayy@gmail.com>
2025-05-21 16:12:07 +0300

    Fix

--- resources/views/frontend/layout/css.blade.php
+++ resources/views/frontend/layout/css.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
 <link rel="stylesheet" href="/frontend/css/style.css?ver=7">
 
-<link rel="alternate" hreflang="tr" href="https://urbanreklam.com/" />
-<link rel="alternate" hreflang="en" href="https://urbanreklam.com/en/" />
-<link rel="alternate" hreflang="ar" href="https://urbanreklam.com/sa/" />
-<link rel="alternate" hreflang="x-default" href="https://urbanreklam.com/" />
+<link rel="alternate" hreflang="tr" href="https://urbanreklam.com.tr/tr" />
+<link rel="alternate" hreflang="en" href="https://urbanreklam.com.tr/en/" />
+<link rel="alternate" hreflang="ar" href="https://urbanreklam.com.tr/sa/" />
+<link rel="alternate" hreflang="x-default" href="https://urbanreklam.com.tr/" />
--- resources/views/frontend/service/detail.blade.php
+++ resources/views/frontend/service/detail.blade.php
@@ -153,7 +153,7 @@
 <style>
     /* Slider CSS */
     .gallery_slider .swiper-slide {
-        height: 500px;
+        height: 700px;
     }
     .gallery_slider .gallery-image {
         width: 100%;
